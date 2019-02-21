8cdb45332a0b43de1f3fdd329031a387f2417d7e
Bazys <bazyssoft@gmail.com>
2019-02-21 18:48:01 +0300

    API url

--- cian.py
+++ cian.py
@@ -4,45 +4,63 @@ from data import data_from_dict
 import os
 import time
 
-URI = 'https://www.cian.ru/cian-api/mobile-site/v2/search-offers/'
+URI = 'https://api.cian.ru/search-offers/v2/search-offers-desktop/'
+file_path = 'output.json'
 
-os.remove('output.txt')
+if os.path.exists(file_path):
+    os.remove(file_path)
+
 
 headers = {
     'Accept': 'application/json',
-    'Content-Type': 'application/json',
+    'Content-Type': 'text/plain;charset=UTF-8',
 }
-
-data = {'region': {'type': 'terms',
-                   'value': [1]
-                   },
-        '_type': 'commercialsale',
-        'category': {'type': 'terms', 'value':['commercialLandSale']},
-        # 'office_type': {'type': 'terms', 'value': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]},
-        'engine_version': {'type': 'term', 'value': 2},
-        'page': {'type': 'term',
-                 'value': 1
-                 }
+data = {
+    "jsonQuery": {
+        "region":
+        {"type": "terms",
+         "value": [4704]
+         },
+        "_type": "commercialsale",
+        # 'category': {'type': 'terms', 'value': ['commercialLandSale']},
+        # 'category': {'type': 'terms', 'value': ['commercialLandSale', 'buildingSale', 'officeSale', 'freeAppointmentObjectSale', 'shoppingAreaSale']},
+        "office_type": {
+            "type": "terms",
+            "value": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+        },
+        "engine_version": {
+            "type": "term",
+            "value": 2
+        },
+        "page": {
+            "type": "term",
+            "value": 1
         }
+    }
+}
 
 response = requests.post(
     URI, headers=headers, data=json.dumps(data))
 
-with open('output.txt', 'a+') as text_file:
-    print(response.text, file=text_file)
+with open(file_path, 'a+', encoding='utf-8') as text_file:
+    print(response.text.encode().decode('unicode-escape'), file=text_file)
+    # print(response.text, file=text_file)
 
 result = data_from_dict(response.json())
 
 print(result.data.offers_serialized)
 
 pages = result.data.aggregated_count // len(result.data.offers_serialized) + 1
+if pages > 60:
+    pages = 60
 
-for x in range(52, pages):
-    data['page']['value'] = x
+for x in range(2, pages):
+    data['jsonQuery']['page']['value'] = x
     response = requests.post(
         URI, headers=headers, data=json.dumps(data))
-    with open('output.txt', 'a+') as text_file:
-        print(response.text, file=text_file)
+    with open(file_path, 'a+', encoding='utf-8') as text_file:
+        print(response.text.encode().decode('unicode-escape'), file=text_file)
+        # print(response.text, file=text_file)
     result = data_from_dict(response.json())
     print(result.data.offers_serialized, x)
     time.sleep(3)
